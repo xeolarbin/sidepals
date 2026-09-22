23f8beb6ad53710387c95ddf6402047585ae0f4b
xeolarbin <brillanteshinetheworld@gmail.com>
2026-09-22 10:41:45 +0000

    main page settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import DiscoverPals from "./(main)/discover-pals/page";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16">
-        <Navbar />
+      <Navbar />
+      <main>
+        <DiscoverPals />
       </main>
+      <Footer />
     </div>
   );
 }
